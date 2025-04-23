04ac89935ae4818041d7b0764369b45fdb60d38c
hendrowunga <hendrowunga073@gmail.com>
2025-04-23 11:43:36 +0700

    Update Component spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideAnimations()],
     }).compileComponents();
   });
 
