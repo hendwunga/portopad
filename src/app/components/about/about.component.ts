e938e8847e4b0f1a13e96e07509297943b9ff398
hendrowunga <hendrowunga073@gmail.com>
2025-04-23 11:19:29 +0700

    Update for About Component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
