472a4e5a8cb9893edf25dfbc293d0f688e687688
hendrowunga <hendrowunga073@gmail.com>
2025-05-08 14:28:19 +0700

    feat; update full hero compnent

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss',
+  styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent {}
+export class HeroComponent {
+  isModalOpen = false;
+
+  profileImageUrl = 'assets/logos/profil.JPG';
+
+  openImage(): void {
+    this.isModalOpen = true;
+    document.body.classList.add('modal-open');
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    document.body.classList.remove('modal-open');
+  }
+}
