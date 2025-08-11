33942a2ca31c04e43584b499bea09629148e8683
hendrowunga <hendrowunga073@gmail.com>
2025-08-11 20:09:52 +0700

    Modification component for footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,33 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faGithub,
   faLinkedin,
-  // faTwitter, // Hapus import Twitter jika tidak digunakan
   faInstagram,
   faTiktok,
   faKaggle,
 } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+import { LanguageService } from '../../services/shared/language.service';
+import { Observable, map } from 'rxjs';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule], // FontAwesomeModule tetap diperlukan untuk fa-icon
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   faGithub = faGithub;
   faLinkedin = faLinkedin;
-  // faTwitter = faTwitter; // Hapus properti Twitter
   faInstagram = faInstagram;
   faTiktok = faTiktok;
   faKaggle = faKaggle;
   faEnvelope = faEnvelope;
 
-  get currentYear(): number {
-    return new Date().getFullYear();
+  currentYear: number = new Date().getFullYear();
+
+  formattedCopyrightText$: Observable<string>;
+
+  constructor(public readonly languageService: LanguageService) {
+    this.formattedCopyrightText$ = this.languageService
+      .getTranslation('footerCopyright')
+      .pipe(
+        map((translation) => {
+          const yearStr = this.currentYear.toString();
+          return translation
+            .replace('%YEAR%', yearStr) // Ganti untuk EN
+            .replace('%TAHUN%', yearStr); // Ganti untuk ID
+        })
+      );
+  }
+
+  ngOnInit(): void {
   }
 }
